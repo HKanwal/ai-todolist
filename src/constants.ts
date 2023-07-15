55c6f48b247946ac454880cfa4e673b9690ee112
Harkeerat Kanwal <hsk532991@gmail.com>
2023-07-15 01:28:11 -0400

    GH pages setup

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-// export const apiUrl = 'https://angular-todolist-server.fly.dev/';
-export const apiUrl = 'http://localhost:3001/';
+export const apiUrl = 'https://angular-todolist-server.fly.dev/';
+// export const apiUrl = 'http://localhost:3001/';

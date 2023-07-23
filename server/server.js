e10ae1e97281a66ce21288b6ef53e3029633fb5e
Harkeerat Kanwal <hsk532991@gmail.com>
2023-07-23 04:11:38 -0400

    Redeploy BE

--- server/server.js
+++ server/server.js
@@ -81,7 +81,7 @@ app.post(["/"], async (req, res) => {
 
 // Endpoint for server testing
 app.get("/test/", (req, res) => {
-  res.send("<h1>Test auto-tagging again</h1>");
+  res.send("<h1>Test auto-tagging again x2</h1>");
 });
 
 app.listen(port, () => console.log(`angular-todolist-server app listening on port ${port}!`));
